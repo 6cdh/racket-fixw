e4a746dffa7e6a187ff3442584ffdb433c9e605e
6cdh <cd6cdh@gmail.com>
2023-03-10 10:31:44 +0800

    process `\r\n` in test

--- tests/test.rkt
+++ tests/test.rkt
@@ -9,9 +9,11 @@
     (for/list ([line (in-port read-line in)])
       line))
 
-  (define original (port->string (open-input-file "expect.rkt")))
-  (define formatted (fixw (open-input-string original) #f))
+  (define port (open-input-file "expect.rkt"))
+  (define code (port->bytes port))
+  (define original-lines (port->lines (open-input-bytes code)))
+  (define formatted (fixw (open-input-bytes code) #f))
   (for ([l1 (string->lines formatted)]
-        [l2 (string->lines original)])
+        [l2 original-lines])
     (check-equal? l1 l2)))
 
